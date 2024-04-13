60509af7b0bb68bf9ef267f852d39a4cfb86bbae
rcaicedom <r.caicedom@uniandes.edu.co>
2024-04-13 12:39:00 -0500

    Add test

--- tests/test_services.py
+++ tests/test_services.py
@@ -1,4 +1,5 @@
 import pytest
+import json
 from unittest.mock import patch
 from functools import wraps
 
@@ -26,5 +27,39 @@ def test_request_ping(client):
     response = client.get("/services/ping")
     assert response.status_code == 200
     assert b"pong" in response.data
+    
+def test_request_post(client):
+     url = "/services"
+     headers = {
+        "Content-Type": "application/json"
+    }
+     data = {
+        "name": "service 1",
+        "description": "description 1",
+        "cost": 20.0,
+        "taxes": 5.0,
+        "address": "direction",
+        "details": "details"
+        }
+     response = client.post(url, data=json.dumps(data), headers=headers)
+     assert response.status_code == 201
+     assert b"name" in response.data
 
+def test_request_get(client):
+     url = "/services"
+     headers = {
+        "Content-Type": "application/json"
+    }
+     data = {
+        "name": "service 1",
+        "description": "description 1",
+        "cost": 20.0,
+        "taxes": 5.0,
+        "address": "direction",
+        "details": "details"
+        }
+     client.post(url, data=json.dumps(data), headers=headers)
+     response = client.get(url)
+     assert response.status_code == 200
+     assert b"services" in response.data
 
